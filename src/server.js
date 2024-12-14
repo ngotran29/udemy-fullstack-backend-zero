ea503f14b5839a0283c1e25a7a2ee31a0eb910c7
ngotran29 <ngotran296yh@gmail.com>
2024-12-15 02:17:37 +0700

    i static file bai21

--- src/server.js
+++ src/server.js
@@ -10,8 +10,12 @@ const hostname = process.env.HOST_NAME;
 //config template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
+
+//config static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 //khai bao route
-app.get('/', (req, res) => {
+app.get('/sample', (req, res) => {
   res.render('sample.ejs')
 })
 
